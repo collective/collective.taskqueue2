a6ae9982e0c45c96469c7b3aaef8d7ebf987f57c
Andreas Jung <yet@gmx.de>
2023-11-24 08:43:05 +0100

    updated

--- src/collective/taskqueue2/huey_config.py
+++ src/collective/taskqueue2/huey_config.py
@@ -1,7 +1,11 @@
+from collective.taskqueue2.huey_logger import LOG
+from huey import FileHuey
+from huey import MemoryHuey
+from huey import RedisHuey
+from huey import SqliteHuey
+
 import furl
 import os
-
-from collective.taskqueue2.huey_logger import LOG
 
 
 default_huey_url = "sqlite:///tmp/huey_queue.sqlite"
@@ -11,37 +15,19 @@ parsed_url = furl.furl(huey_url)
 scheme = parsed_url.scheme
 
 if scheme == "sqlite":
-    from huey import SqliteHuey
-
     huey_taskqueue = SqliteHuey(filename=str(parsed_url.path))
-
 elif scheme == "redis":
-    # requires redis-p
-    from huey import RedisHuey
-    try:
-        pass
-    except Exception:
-        msg = "For using Redis, you need to install py-redis"
-        raise RuntimeError(msg)
-
+    # requires redis-py
     huey_taskqueue = RedisHuey(
         host=parsed_url.host,
         port=parsed_url.port,
         password=parsed_url.password,
         db=int(str(parsed_url.path).lstrip("/")),
     )
-
 elif scheme == "memory":
-    # requires redis-py
-    from huey import MemoryHuey
-
     huey_taskqueue = MemoryHuey()
-
 elif scheme == "file":
-    from huey import FileHuey
-
     huey_taskqueue = FileHuey(path=str(parsed_url.path))
-
 else:
     raise ValueError(
         f"No proper configuration for $HUEY_TASKQUEUE_URL found ({huey_url}"
