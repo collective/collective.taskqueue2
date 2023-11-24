b1c90e6dff551ccfa5e79b2e19184a9274437f15
Andreas Jung <yet@gmx.de>
2023-11-24 08:48:38 +0100

    minor refactoring

--- src/collective/taskqueue2/huey_config.py
+++ src/collective/taskqueue2/huey_config.py
@@ -10,27 +10,33 @@ import os
 
 default_huey_url = "sqlite:///tmp/huey_queue.sqlite"
 
-huey_url = os.environ.get("HUEY_TASKQUEUE_URL", default_huey_url)
-parsed_url = furl.furl(huey_url)
-scheme = parsed_url.scheme
+def get_huey_taskqueue():
+    """ Return a Huey taskqueue instance """
 
-if scheme == "sqlite":
-    huey_taskqueue = SqliteHuey(filename=str(parsed_url.path))
-elif scheme == "redis":
-    # requires redis-py
-    huey_taskqueue = RedisHuey(
-        host=parsed_url.host,
-        port=parsed_url.port,
-        password=parsed_url.password,
-        db=int(str(parsed_url.path).lstrip("/")),
-    )
-elif scheme == "memory":
-    huey_taskqueue = MemoryHuey()
-elif scheme == "file":
-    huey_taskqueue = FileHuey(path=str(parsed_url.path))
-else:
-    raise ValueError(
-        f"No proper configuration for $HUEY_TASKQUEUE_URL found ({huey_url}"
-    )
+    huey_url = os.environ.get("HUEY_TASKQUEUE_URL", default_huey_url)
+    parsed_url = furl.furl(huey_url)
+    scheme = parsed_url.scheme
+
+    if scheme == "sqlite":
+        return  SqliteHuey(filename=str(parsed_url.path))
+    elif scheme == "redis":
+        # requires redis-py
+        return RedisHuey(
+            host=parsed_url.host,
+            port=parsed_url.port,
+            password=parsed_url.password,
+            db=int(str(parsed_url.path).lstrip("/")),
+        )
+    elif scheme == "memory":
+        return MemoryHuey()
+    elif scheme == "file":
+        return FileHuey(path=str(parsed_url.path))
+    else:
+        raise ValueError(
+            f"No proper configuration for $HUEY_TASKQUEUE_URL found ({huey_url}"
+        )
+
+
+huey_taskqueue = get_huey_taskqueue()
 
 LOG.info(f"Using taskqueue {huey_taskqueue}, {huey_taskqueue.__dict__}")
